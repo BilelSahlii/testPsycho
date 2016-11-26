be81c4ae6f5d4752cbe796440c0dd418f2967de9
bilel <bilel@bilel-PC>
2016-11-26 20:40:46 +0100

    first commit

---
+++ Smart-Psycho/src/main/java/com/time/smartpsycho/SmartPsychoApplication.java
@@ -0,0 +1,12 @@
+package com.time.smartpsycho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartPsychoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartPsychoApplication.class, args);
+	}
+}
---
+++ Smart-Psycho/src/test/java/com/time/smartpsycho/SmartPsychoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.time.smartpsycho;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmartPsychoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
